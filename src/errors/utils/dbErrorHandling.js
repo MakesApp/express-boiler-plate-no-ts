adbb1b9916d1d40aeb8d19e2606dd488cba6dc2b
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2023-11-27 16:00:51 +0100

    updated dbErrorHandling bug

--- src/errors/utils/dbErrorHandling.js
+++ src/errors/utils/dbErrorHandling.js
@@ -1,13 +1,17 @@
 import AppError from '../AppError.js'; // Update the path as needed
 
 export function applyErrorHandlingMiddleware(schema) {
-  schema.post('save', (error, next) => {
-    if (error.name === 'ValidationError') {
-      next(new AppError('Validation error', 400, true));
-    } else if (error.name === 'MongoError' && error.code === 11000) {
-      next(new AppError('Duplicate key error', 409, true));
+  schema.post('save', (error, doc, next) => {
+    if (error) {
+      if (error.name === 'ValidationError') {
+        next(new AppError('Validation error', 400, true));
+      } else if (error.name === 'MongoError' && error.code === 11000) {
+        next(new AppError('Duplicate key error', 409, true));
+      } else {
+        next(error);
+      }
     } else {
-      next(error);
+      next();
     }
   });
 }
